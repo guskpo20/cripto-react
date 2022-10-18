b353466c3a47e5ef4a82c850b007e1c2500b3f54
Gustavo <75509324+guskpo20@users.noreply.github.com>
2022-10-18 00:09:11 -0300

    Inputs listos

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: 'USD', nombre: 'Dolar de Estados Unidos' },
+  { id: 'UYU', nombre: 'Pesos Uruguayos' },
+  { id: 'EUR', nombre: 'Euros' },
+  { id: 'GBP', nombre: 'Libra Esterlina' },
+];
+
+export { monedas };
